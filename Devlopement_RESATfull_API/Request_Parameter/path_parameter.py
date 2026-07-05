efce01bb020737143de1dee9a5952539b028c723
Mahdi88BH <elmahdibahadia@gmail.com>
2026-07-05 13:55:12 -0400

    Path Parameter

---
+++ Devlopement_RESATfull_API/Request_Parameter/path_parameter.py
@@ -0,0 +1,26 @@
+from enum import Enum
+from fastapi import FastAPI, Path
+
+
+class usetType(str, Enum):
+    ADMIN = "admin"
+    CLIENT = "client"
+    
+
+app = FastAPI()
+
+
+@app.get('/users/{role}/{id}')
+def get_user(role: usetType, id: int = Path(..., gt=1)) -> dict:
+    return {"retrive": {
+        'id': id,
+        "role": role,
+    }}
+
+
+@app.get('/licenses/{license}')
+def get_license(license: str = Path(..., 
+                                    max_length=9, 
+                                    min_length=9,
+                                    regex=r"^\w{2}-\d{3}-\w{2}$")) -> dict:
+    return {"lisence": license}
