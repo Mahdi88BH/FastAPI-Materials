1d50622bfc14bb5202a4fa6434b469e68b2ed78c
Mahdi88BH <elmahdibahadia@gmail.com>
2026-08-29 12:47:26 -0400

    Partially Updating

--- 2-Managing_Pydantic_Model/3-Working_with_Pydantic_Objects.py
+++ 2-Managing_Pydantic_Model/3-Working_with_Pydantic_Objects.py
@@ -29,6 +29,30 @@ class Person(BaseModel):
         return self.model_dump(include={"first_name", "last_name"})
 
 
+
+class PostBase(BaseModel):
+    title: str
+    content: str
+
+class PostCreate(PostBase):
+    pass
+
+class PostRead(PostBase):
+    id: int
+
+class Post(PostBase):
+    id: int
+    nb_views: int = 0
+
+class PostPartialUpdate(BaseModel):
+    title: str | None = None
+    content: str | None = None
+
+
+
+
+
+
 if __name__ == "__main__":
 
     person = Person(
@@ -66,3 +90,28 @@ if __name__ == "__main__":
     print(person_nested_include)
 
     print(person.name_dict())
+
+
+    # Creating an instance from a sub-class object
+    # @app.post("/posts", 
+    #           status_code=status.HTTP_201_CREATED, 
+    #           response_model=PostRead)
+    # async def create(post_create: PostCreate):
+    #     new_id = max(db.posts.keys() or (0,)) + 1
+    #     post = Post(id=new_id, **post_create.model_dump())
+    #     db.posts[new_id] = post
+    #     
+    #     return post
+
+
+    # Updating an instance partially
+    #     @app.patch("/posts/{id}", response_model=PostRead)
+    #     async def partial_update(id: int, post_update: PostPartialUpdate):
+    #     try:
+    #         post_db = db.posts[id]
+    #         updated_fields = post_update.model_dump(exclude_unset=True)
+    #         updated_post = post_db.copy(update=updated_fields)
+    #         db.posts[id] = updated_post
+    #         return updated_post
+    #     except KeyError:
+    #         raise HTTPException(status.HTTP_404_NOT_FOUND)
