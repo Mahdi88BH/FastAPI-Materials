ea915c645150d0e7569590439ca04039d8cbd4cf
Mahdi88BH <elmahdibahadia@gmail.com>
2026-08-29 14:36:06 -0400

    Dependencies

---
+++ 3-Dependency_Injection/3-Dependencies.py
@@ -0,0 +1,31 @@
+from fastapi import Header, FastAPI, Depends, HTTPException, status, APIRouter
+
+
+
+
+def secret_header(secret_header: str | None = Header(None)) -> None:
+    if not secret_header or secret_header != "SECRET_VALUE":
+        raise HTTPException(status.HTTP_403_FORBIDDEN)
+
+
+# Using a dependency on a whole application
+app = FastAPI(dependencies= [Depends(secret_header)])
+
+router = APIRouter(dependencies= [Depends(secret_header)])
+app.include_router(router, prefix="/router")
+
+
+# Using a dependency on a path decorator
+@app.get("/protected-route", dependencies= [Depends(secret_header)])
+async def protected_route():
+    return {"hello": "world"}
+
+
+# Using a dependency on a whole router
+@router.get("/route1")
+async def router_route1():
+        return {"route": "route1"}
+
+@router.get("/route2")
+async def router_route2():
+        return {"route": "route2"}
