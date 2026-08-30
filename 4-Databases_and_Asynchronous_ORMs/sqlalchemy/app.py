803debf53e9a52da816ccddb3e7d12573233e0ae
Mahdi88BH <elmahdibahadia@gmail.com>
2026-08-30 14:53:34 -0400

    Creating Objects

--- 4-Databases_and_Asynchronous_ORMs/sqlalchemy/app.py
+++ 4-Databases_and_Asynchronous_ORMs/sqlalchemy/app.py
@@ -1,7 +1,10 @@
 import contextlib
 
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends, status
+from sqlalchemy.ext.asyncio import AsyncSession
 
+import schemas
+from models import Post
 from database import create_all_tables
 
 
@@ -14,3 +17,14 @@ async def lifespan(app: FastAPI):
 app = FastAPI(lifespan=lifespan)
 
 
+
+@app.post('/posts', response_model=schemas.PostRead, status_code=status.HTTP_201_CREATED)
+async def create_post(
+    post_create: schemas.PostCreate, session: AsyncSession = Depends
+) -> Post:
+
+    post = Post(**post_create.model_dump())
+    session.add(post)
+    await session.commit()
+
+    return post
